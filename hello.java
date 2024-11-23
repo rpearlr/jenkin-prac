e9d72791ee1af790a5728200fcac2362c241678e
rpearlr <140599004+rpearlr@users.noreply.github.com>
2024-11-23 21:11:24 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+
+// Driver Class
+class hello {
+    // main function
+    public static void main(String[] args)
+    {
+       System.out.println("Hello");
+    }
+}
